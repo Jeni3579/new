fd4814e5e56be59c6c7e5dfae63112b7c581f1be
Allan0092 <allan.gtm92@gmail.com>
2022-10-14 09:44:46 +0545

    filter even

--- first/first.py
+++ first/first.py
@@ -1,1 +1,10 @@
-print("Hello world")
+# Filter Even number
+
+def filter_even(_num):
+	for n in _num:
+		if n%2!=0:
+			_num.pop(_num.index(n))
+	return _num
+
+num=[1,2,3,4,5,6,7,8,9]
+print(filter_even(num))
